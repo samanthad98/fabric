09d56cf04acbb78eb1ec6256cc2938973a3e35e9
yd246 <yd246@cornell.edu>
2018-10-22 11:35:44 -0400

    Added backoffcase enum and constructors in FabricRuntimeException

--- src/system/fabric/common/exceptions/FabricRuntimeException.java
+++ src/system/fabric/common/exceptions/FabricRuntimeException.java
@@ -1,19 +1,44 @@
 package fabric.common.exceptions;
 
 public class FabricRuntimeException extends RuntimeException {
+  BackoffCase b;
+  
   public FabricRuntimeException() {
     super();
+    b = BO;
   }
 
   public FabricRuntimeException(String message) {
     super(message);
+    b = BO;
+  }
+  
+  public FabricRuntimeException(String message, BackoffCase b) {
+    super(message);
+    self.b = b;
   }
 
   public FabricRuntimeException(Throwable cause) {
     super(cause);
+    b = BO;
+  }
+  
+  public FabricRuntimeException(Throwable cause, BackoffCase b) {
+    super(cause);
+    self.b = b;
   }
 
   public FabricRuntimeException(String message, Throwable cause) {
     super(message, cause);
+    b = BO;
+  }
+  
+  public FabricRuntimeException(String message, Throwable cause, BackoffCase b) {
+    super(message, cause);
+    self.b = b;
+  }
+  
+  enum BackoffCase {
+    Palse, BOnon, BO;
   }
 }
